2f1e097019e4f27a249ed88129b8e94f3afa0df9
Jp Gallegos <jp.gallegos.diaz@gmail.com>
2014-03-17 15:08:20 -0400

    eManager: Working on Authorization, but first trying to figure out how to delete from DB. MvcMovie: almost finished Getting Started with ASP.NET MVC4 -> Examining the editmethods and edit view.

---
+++ eManager/eManager.Web/Controllers/DepartmentController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using eManager.Domain;
+using eManager.Web.Infrastructure;
+
+namespace eManager.Web.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private IDepartmentDataSource _db = new DepartmentDb();
+
+        public ActionResult Detail(int id)
+        {
+            var model = _db.Departments.Single(d => d.Id == id);
+            return View(model);
+        }
+
+    }
+}
---
+++ eManager/eManager.Web/Controllers/EmployeeController.cs
@@ -0,0 +1,49 @@
+﻿using eManager.Domain;
+using eManager.Web.Infrastructure;
+using eManager.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eManager.Web.Controllers
+{
+    //[Authorize(Roles="Admin")]
+    public class EmployeeController : Controller
+    {
+        private IDepartmentDataSource _db = new DepartmentDb();
+
+        [HttpGet]
+        public ActionResult Create(int departmentId)
+        {
+            var model = new CreateEmployeeViewModel();
+            model.DepartmentId = departmentId;
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Create(CreateEmployeeViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var department = _db.Departments.Single(d => d.Id == viewModel.DepartmentId);
+                var employee = new Employee();
+                employee.Name = viewModel.Name;
+                employee.HireDate = viewModel.HireDate;
+                department.Employees.Add(employee);
+
+                _db.Save();
+
+                return RedirectToAction("detail", "department", new { id = viewModel.DepartmentId });
+            }
+            return View(viewModel);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+    }
+}
